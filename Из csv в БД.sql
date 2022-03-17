024f4d543b9f5875905d4e91307a73906bd24f4a
khusamov01@icloud.com <a.husamov@yandex.ru>
2022-03-17 11:45:26 +0500

    v0.0.4

---
+++ Из csv в БД.sql
@@ -0,0 +1,16 @@
+﻿IF (OBJECT_ID('CSV_Export') IS NOT NULL) DROP TABLE dbo.CSV_Export;
+
+CREATE TABLE CSV_Export (
+	[id]             INT            IDENTITY (1, 1) NOT NULL,
+    [Название книги] NVARCHAR (MAX) NULL,
+    [Автор книги]    NVARCHAR (MAX) NULL,
+    [Дата издания]   NVARCHAR (4)   NULL,
+    [Издательство]   NVARCHAR (MAX) NULL,
+    [Обложка]        NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+)
+
+BULK INSERT dbo.CSV_Export
+FROM 'C:\Users\User\data_books.csv'
+WITH (fieldterminator = ';', rowterminator = '\n');
+
